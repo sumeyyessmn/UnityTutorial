bc1628bd4be29d06114865d920603ac96fbf7ebb
Sumeyye Ssmn <sumeyye.ssmn@gmail.com>
2025-02-22 17:46:08 +0300

    array & list

--- Assets/Scripts/TestingScript.cs
+++ Assets/Scripts/TestingScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TestingScript : MonoBehaviour
 {
@@ -26,13 +27,36 @@ public class TestingScript : MonoBehaviour
         Debug.Log("Awake");
     }
     //Start fonksiyonu, oyun başlarken çalışan, 1 kere çalışan bir fonksiyondur.
+    int[] numbers = {1,2,3,4,5};
+    public static List<int> numbersList = new List<int>();
+    
+    // int[] numbers = new int[3]; //3 tane eleman oluştur içlerini boş bırak
     void Start()
     {
-        score = 0;
-        Debug.Log("Start");
-        TestFunction();
-        //Debug.LogWarning("Warning Log!"); sarı, uyarı
-        //Debug.LogError("Error Log!"); kırmızı, hata
+       
+        // score = 0;
+        // Debug.Log("Start");
+        // TestFunction();
+        // //Debug.LogWarning("Warning Log!"); sarı, uyarı
+        // //Debug.LogError("Error Log!"); kırmızı, hata
+        // //Array
+        
+        // //Debug.Log(numbers[2]);
+        // // numbers[0] = 2;
+        // // numbers[1] = 3;
+        // // numbers[2] = 4;
+        // for(int i = 0; i < numbers.Length; i++)
+        // {
+        //     Debug.Log(numbers[i]);
+        // }
+        numbersList.Add(13);
+        numbersList.Add(15);
+        numbersList.Add(17);
+        PrintList();
+        numbersList.Remove(13);
+        PrintList();
+        numbersList.Clear();
+        PrintList();
     }
     void Update()
     {
@@ -47,6 +71,19 @@ public class TestingScript : MonoBehaviour
     void TestFunction()
     {
         Debug.Log("Test Function");
+    }
+    void PrintList()
+    {
+        // for (int i = 0; i < numbersList.Count;i++)
+        // {
+        //     Debug.Log(numbersList[i]);
+            
+        // }
+        foreach(int myNumber in numbersList)
+        {
+            Debug.Log(myNumber);
+        }
+        Debug.Log("-------------------");
     }
 }
 
